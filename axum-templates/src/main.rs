acded1b39f766d3249887ba7e88401d52c549ca5
kentakom1213 <kentakom1213@gmail.com>
2023-01-06 16:55:22 +0900

    [add] ログの出力設定

--- axum-templates/src/main.rs
+++ axum-templates/src/main.rs
@@ -7,26 +7,27 @@ use axum::{
     Router,
 };
 use std::net::SocketAddr;
-// use tracing_subscriber::{
-//     layer::SubscriberExt,
-//     util::SubscriberInitExt, fmt::init,
-// };
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
 
 #[tokio::main]
 async fn main() {
-    // tracing_subscriber::registry()
-    //     .with(
-    //         tracing_subscriber::EnvFilter::try_from_default_env()
-    //             .unwrap_or_else(|_| "example_templates=debug".into()),
-    //     )
-    //     .with(tracing_subscriber::fmt()::layer())
-    //     .init();
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "example_templates=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
     // ルーティングしながらビルド
     let app = Router::new().route("/greet/:name", get(greet));
 
     // 実行
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    // デバッグログ
+    tracing::debug!("listening on {}", addr);
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
